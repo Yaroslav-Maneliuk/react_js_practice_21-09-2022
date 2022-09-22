f0d21455320ce8e1f3b80ceb603b6ae8f9aff739
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-09-22 19:52:40 +0300

    Add state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-const App = () => {
-  return <div></div>;
-};
+import { useState, useEffect } from 'react'
+import './App.css'
 
-export default App;
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    setCounter(100)
+  }, [])
+
+  return (
+    <div className='App'>
+      <button onClick={() => setCounter(prevCount => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(prevCount => prevCount + 1)}>+</button>
+    </div>
+  )
+}
+
+export default App
